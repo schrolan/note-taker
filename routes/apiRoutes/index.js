e69dc1840bf1cf60331a6d012da4bd22d030b031
schrolan <schrolan000@gmail.com>
2023-02-08 18:44:58 -0600

    trying to get it to work

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
       });
     
-      // If no matching route is found default to index
       app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
       });
