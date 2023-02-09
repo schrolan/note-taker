1bef10c430f65a112a23fbda82effdf255e69309
schrolan <schrolan000@gmail.com>
2023-02-08 19:04:29 -0600

    I did all I could for now

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(routes)
 
-require('./routes/apiroutes')(app);
-require('./routes/htmlroutes')(app);
+require('./routes/apiRoutes/api.js')(app);
+require('./routes/index.js')(app);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
